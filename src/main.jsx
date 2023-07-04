5bbd0f8a2174fd931b8ce967094030b78c3a3031
lance <lanceparker2001@gmail.com>
2023-07-04 18:50:46 +0530

    server changed from local to deployed

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import "./styles/app.scss";
 import { AppContextProvider } from "./context/AppContext.jsx";
 
 //Production
-// export const server = "https://backend-todo-app-gt1s.onrender.com/api/v1";
+export const server = "https://backend-todo-app-gt1s.onrender.com/api/v1";
 //Development
-export const server = "http://localhost:5000/api/v1";
+// export const server = "http://localhost:5000/api/v1";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppContextProvider>
